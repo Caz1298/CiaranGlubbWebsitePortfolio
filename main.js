c8013cd713ab9b7394e9d246630322e0d76857fc
Caz1298 <ciaran.glubb@gmail.com>
2026-08-28 16:46:01 +1200

    update

--- main.js
+++ main.js
@@ -3,51 +3,77 @@ const video2 = document.querySelector("#Video2Container video");
 const video3 = document.querySelector("#Video3Container video");
 const video4 = document.querySelector("#Video4Container video");
 
-const OuterVideocontainer1 = document.getElementById("Video1")
-const OuterVideocontainer2 = document.getElementById("Video2")
-const OuterVideocontainer3 = document.getElementById("Video3")
-const OuterVideocontainer4 = document.getElementById("Video4")
+const Videocontainer1 = document.getElementById("Video1Container")
+const Videocontainer2 = document.getElementById("Video2Container")
+const Videocontainer3 = document.getElementById("Video3Container")
+const Videocontainer4 = document.getElementById("Video4Container")
+
+const OtherGamescontainer = document.getElementById("OtherGamesContainer")
+
+const hoverText1 = document.getElementById("HoverText1")
+const hoverText2 = document.getElementById("HoverText2")
+const hoverText3 = document.getElementById("HoverText3")
+const hoverText4 = document.getElementById("HoverText4")
+const hoverText5 = document.getElementById("HoverText5")
 
 video1.addEventListener("mouseenter", () => {
     video1.play();
-    OuterVideocontainer1.classList.add("active")
+    Videocontainer1.classList.add("active")
+    hoverText1.classList.remove("active")
 })
 
 video1.addEventListener("mouseleave", () => {
-    video1.pause();
     video1.currentTime = 0;
-    OuterVideocontainer1.classList.remove("active")
+    video1.pause();
+    Videocontainer1.classList.remove("active")
+    hoverText1.classList.add("active")
 })
 
 video2.addEventListener("mouseenter", () => {
     video2.play();
-    OuterVideocontainer2.classList.add("active")
+    Videocontainer2.classList.add("active")
+    hoverText2.classList.remove("active")
 })
 
 video2.addEventListener("mouseleave", () => {
-    video2.pause();
     video2.currentTime = 0;
-    OuterVideocontainer2.classList.remove("active")
+    video2.pause();
+    Videocontainer2.classList.remove("active")
+    hoverText2.classList.add("active")
 })
 
 video3.addEventListener("mouseenter", () => {
     video3.play();
-    OuterVideocontainer3.classList.add("active")
+    Videocontainer3.classList.add("active")
+    hoverText3.classList.remove("active")
 })
 
 video3.addEventListener("mouseleave", () => {
-    video3.pause();
     video3.currentTime = 0;
-    OuterVideocontainer3.classList.remove("active")
+    video3.pause();
+    Videocontainer3.classList.remove("active")
+    hoverText3.classList.add("active")
 })
 
 video4.addEventListener("mouseenter", () => {
     video4.play();
-    OuterVideocontainer4.classList.add("active")
+    Videocontainer4.classList.add("active")
+    hoverText4.classList.remove("active")
 })
 
 video4.addEventListener("mouseleave", () => {
-    video4.pause();
     video4.currentTime = 0;
-    OuterVideocontainer4.classList.remove("active")
+    video4.pause();
+    Videocontainer4.classList.remove("active")
+    hoverText4.classList.add("active")
+})
+
+OtherGamescontainer.addEventListener("mouseenter", () => {
+    OtherGamescontainer.classList.add("active")
+    hoverText5.classList.remove("active")
+})
+
+OtherGamescontainer.addEventListener("mouseleave", () => {
+    OtherGamescontainer.classList.remove("active")
+    hoverText5.classList.add("active")
 })
